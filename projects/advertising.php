e1721f610abb78ce4dbc6b7a674c2dac1ea6207c
Shridhar Sundaram <ssundaram1000@gmail.com>
2016-09-16 09:38:46 -0400

    new files added

---
+++ projects/advertising.php
@@ -0,0 +1,141 @@
+ 
+<!-- ROW 1 -->
+
+					<article class="project-item advertising">
+						
+						<img class="img-responsive project-image" src="images/advertising/tn/ski_busshelter_tn.jpg"
+						 alt=""><!--Project thumb -->
+						
+						<div class="hover-mask">
+							<h2 class="project-title">Colorado Ski Resort</h2><!--Project Title -->
+							<p>Advertising Design</p><!--Project Subtitle -->
+						</div>
+						
+						<!--==== Project Preview HTML ====-->
+						
+						<div class="sr-only project-description" data-images="images/advertising/ski_billboard_modified.png,images/advertising/ski_busshelter.jpg" >
+						
+							<h2>Requirement</h2>
+							<p>
+								Create 2 ad designs for a Colorado Ski Resort, for 2 different locations, one for a bus shelter and the other for a billboard. Show ability to produce your own original slogan and copy to artfully incorporate into your ad designs
+							</p>
+							<p>
+								Original slogan used for the campaign: The inspiration for this slogan was from Ringling Bros Circus. I modified it with a play on words “The Greatest Show on Earth” – The h in Show is a strike-through to show ‘N’ for Snow. Intent was to catch the attention and have basically 2 slogans at the same time.
+								
+							</p>
+							
+							<h2>TECHNIQUES USED</h2>
+							<p>
+								I wanted to show the excitement and thrill of skiing down beautiful powder snow as well as emphasize that this was for both experienced, beginner and families. For visual hierarchy on “copy”, I ordered them by importance as shown below. The first of them below was to grab attention to the ad. This is especially important for a billboard. All the 4 below should be common across the ads. The 5th item is only for the bus shelter, since it requires more time to read 
+								<ul>
+								<li>The greatest snow on earth </li>
+								<li>Slopes for all skill levels</li>
+								<li>Affordable Packages with Lessons</li>
+								<li>Directions to get there (Exit 17 off Hwy 85)</li>
+								<li>Snowmobiling, Golf and Fine Dining (Only for bus shelter) I kept the typeface and colors of type same across the two ads. I also kept logo and one of the images same to keep consistency and familiarity.</li>
+								</ul>					
+							</p>
+
+
+						</div>
+						
+					</article><!--End Project Item - Ski Slopes -->	
+
+					<article class="project-item advertising">
+						
+						<img class="img-responsive project-image" src="images/advertising/tn/yogurt_ad_tn.jpg"
+						 alt=""><!--Project thumb -->
+						
+						<div class="hover-mask">
+							<h2 class="project-title">Streamliner Yogurt AD</h2><!--Project Title -->
+							<p>Advertising Design</p><!--Project Subtitle -->
+						</div>
+						
+						<!--==== Project Preview HTML ====-->
+						
+						<div class="sr-only project-description" data-images="images/advertising/yogurt_ad_summer.jpg,images/advertising/yogurt_ad_winter.jpg" >
+						
+							<h2>Requirement</h2>
+							<p>
+								Develop designs for “Summer” and a “Winter” Advertisements for Streamliner Yogurt which emphasize specific qualities of the product and create a familiarity with the brand. 
+								Demonstrate the ability to conceptualize an ad campaign by presenting in a written brief that applied differently /similarly to each ad publication, and season.
+							</p>
+							
+							<h2>TECHNIQUES USED</h2>
+							<p>
+								For the <strong>summer ad</strong>, I chose an active family across 3 generations. This is important since it shows bone health is needed while you age. Glamour Magazine’s audience is mostly women between the ages of 18-50, who will be beginning their careers, family and kids. So looks, health and lifestyle is what they are focusing on, so a family summer vacation at the beach fit perfectly for this. 
+								
+							</p>
+							<p>
+								For the <strong>winter ad</strong>, I chose an active youth playing on ski slopes. It shows high energy and active lifestyle, which connects with our target audience. People Magazine has the largest subscription, with audience spanning all ages and gender, so picked a winter sport that most people would like to do in their lifetime, wherever they live. 
+							</p>
+
+						</div>
+						
+					</article><!--End Project Item - Telemobile -->	
+
+					<article class="project-item advertising">
+						
+						<img class="img-responsive project-image" src="images/advertising/tn/stranded_ny_tn.jpg"
+						 alt=""><!--Project thumb -->
+						
+						<div class="hover-mask">
+							<h2 class="project-title">Stranded in NY TV Series</h2><!--Project Title -->
+							<p>Advertising Design</p><!--Project Subtitle -->
+						</div>
+						
+						<!--==== Project Preview HTML ====-->
+						
+						<div class="sr-only project-description" data-images="images/advertising/stranded_ny.jpg" >
+						
+							<h2>Requirement</h2>
+							<p>
+								Develop three different ads: a coffee cup, a magazine ad, and a TV commercial, for a new television reality show.
+								Demonstrate the ability to balance the client’s brand (RTN) with the product’s brand (“Stranded in New York”)				
+							</p>
+							
+							<h2>TECHNIQUES USED</h2>
+							<p>
+								Magazine AD Stranded in New York is the new survivor show on RTN. I wanted to create the excitement that the client was looking for. Used an image of 7 people, with focus on one individual who is showing his empty pockets which is the main idea of the show be able to survive with 7 strangers, only on $100 to share. Placed them in Times Square, indicating that they have to survive in New York. Idea was to have the same/similar contestant image and a different city background for each season as the gets popular. Colors: Used a pink color for RTN logo placed it on the right side where the eye would naturally fall. Made “Stranded” the same color as the RTN Circle color and “in New York” in a dark blue color. Used highlighter colors for the details of the shows “7 Strangers…..win 25K” to have interested folks read more Included the timing “Sundays 9/8c” on a banner, placed on the building, used a blue color to This ad will make people curious to see how these people survive and who wins after 7 days.							
+							</p>
+
+
+						</div>
+						
+					</article><!--End Project Item - Telemobile -->				
+
+<!-- ROW 2 -->
+
+
+
+
+					<article class="project-item advertising">
+						
+						<img class="img-responsive project-image" src="images/advertising/tn/telemobile_tn.png"
+						 alt=""><!--Project thumb -->
+						
+						<div class="hover-mask">
+							<h2 class="project-title">Two Tone Mobile Company Subway AD</h2><!--Project Title -->
+							<p>Advertising Design</p><!--Project Subtitle -->
+						</div>
+						
+						<!--==== Project Preview HTML ====-->
+						
+						<div class="sr-only project-description" data-images="images/advertising/telemobile_1.png,images/advertising/telemobile_2.png" >
+						
+							<h2>Requirement</h2>
+							<p>
+								Create two designs for a subway advertisement for a cell phone service provider, one utilizing a humorous and the other a dramatic tone.
+							</p>
+							
+							<h2>TECHNIQUES USED</h2>
+							<p>
+								Created two ads for Telemobile, picked a bright pink color for all the important text &amp; logo on both ads. For the Dramatic one: used a young women literally throwing away $$$ on her poor cell phone service and showed other young people smiling and enjoying a conversation with their smart phones.								
+							</p>
+							<p>
+								For the Humorous one: used a Bright Side Ad theme - This shows the bright side of paying high dollar for poor and dropped cell services with the competition. I used people pointing their phones to the sky to get a signal and used the words bright side of NO SERVICE is to see the blue sky VS enjoy Telemobile service on a beach with extensive coverage. 
+							</p>
+
+						</div>
+						
+					</article><!--End Project Item - Telemobile -->	
